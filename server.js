0e75b9134b422d220471ecd714352e9aa1b8de19
Ivan N. Veselov <veselov@gmail.com>
2011-02-06 16:21:53 +0200

    Added 'nick' command handler on server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http'), 
+var http = require('http'),
     io = require('socket.io'),
     players = {};
 
@@ -6,35 +6,59 @@ var http = require('http'),
 var server = http.createServer();
 var socket = io.listen(server);
 
+function cmdHandler(message, client) {
+    var cmdName = message.name;
+    console.log('got cmd: ' + cmdName);
+    switch (cmdName) {
+        case 'nick':
+        players[client.sessionId].nick = message.arg;
+        console.log('nick has been changed to ' + message.arg + ' for player ' + client.sessionId);
+        break;
+
+        default:
+        console.log('no such cmd defined yet: ' + cmdName);
+    };
+}
+
 // Add a connect listener
-socket.on('connection', function(client) { 
+socket.on(
+    'connection',
+    function(client) {
+        // Send to the new user the list of active players
+        client.send({ type: 'playerslist', list: players });
 
-              // Send to the new user the list of active players
-              client.send({ type: 'playerslist', list: players });
+        // Add the new user to the list of players
+        players[client.sessionId] = { nick: 'anonymous' };
 
-              // Add the new user to the list of players
-              players[client.sessionId] = { name: 'newbie' };
+        // Broadcast the new user to all players
+        socket.broadcast({ type: 'new', id: client.sessionId }, [client.sessionId]);
 
-              // Broadcast the new user to all players
-              socket.broadcast({ type: 'new', id: client.sessionId }, [client.sessionId]);
+        client.on(
+            'message',
+            function(message) {
+                console.log('got message: ' + JSON.stringify(message));
 
-              client.on('message', function (message) {
-                            if (message.type != 'name') {
-                                return;
-                            }
-                            console.log('setting name of ' + message.id + ' to ' + message.name);
-                            players[message.id] = { name: message.name };
-                            client.broadcast(message, [message.id]);
-                        });
+                switch (message.type) {
+                    case "cmd":
+                    cmdHandler(message, client);
+                    break;
 
-              client.on('disconnect', function () {
-                            // Remove the user from the list of players
-                            delete players[this.sessionId];
+                    default:
+                    console.log('no handler specified yet for message.type = ' + message.type);
+                }
+            });
 
-                            // Broadcast the logged out user's id
-                            socket.broadcast({ type: 'leave', id: this.sessionId });
-                        });
-	      });
+        client.on(
+            'disconnect',
+            function () {
+                // Remove the user from the list of players
+                delete players[this.sessionId];
+
+                // Broadcast the logged out user's id
+                socket.broadcast({ type: 'leave', id: this.sessionId });
+            });
+    });
 
 // start listening
 server.listen(8124);
+

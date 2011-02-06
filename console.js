70751eec8afc453938469c8fc50d9c2788489f77
ILLegaL <vitaliy.tyutyunnik@gmail.com>
2011-02-06 17:20:47 +0200

    Implemented new commands: invite, accept, decline.

--- console.js
+++ console.js
@@ -6,6 +6,9 @@ var PORT = 8124;
 
 var TYPE_COMMAND = "cmd";
 var CMD_NICK = "nick";
+var CMD_INVITE = "invite";
+var CMD_ACCEPT = "accept";
+var CMD_DECLINE = "decline";
 
 var socket;
 
@@ -15,7 +18,8 @@ function send(dataStr) {
         socket.send(message);
         output.append("\nclient: Sent object " + JSON.stringify(message));
     } else {
-        // TODO: show help
+        output.append("\nclient: Syntax -- /&lt;command&gt; [arg1, arg2, ... ]");
+        output.append("\nclient: Commands available -- nick, invite, accept, decline");
     }
 }
 
@@ -25,9 +29,27 @@ function parseCommand(commandStr) {
     if (commandElements.length > 0 && commandElements[0].charAt(0) === '/') {
         var commandName = commandElements[0].slice(1);
         switch (commandName) {
-        case "nick":
+        case CMD_NICK:
             if (commandElements[1] && commandElements[1].length > 0) {
                 message = { type : TYPE_COMMAND, name : CMD_NICK, arg : commandElements[1] };
+            }
+            break;
+
+        case CMD_INVITE:
+            if (commandElements[1] && commandElements[1].length > 0) {
+                message = { type : TYPE_COMMAND, name : CMD_INVITE, arg : commandElements[1] };
+            }
+            break;
+
+        case CMD_ACCEPT:
+            if (commandElements[1] && commandElements[1].length > 0) {
+                message = { type : TYPE_COMMAND, name : CMD_ACCEPT, arg : commandElements[1] };
+            }
+            break;
+
+        case CMD_DECLINE:
+            if (commandElements[1] && commandElements[1].length > 0) {
+                message = { type : TYPE_COMMAND, name : CMD_DECLINE, arg : commandElements[1] };
             }
             break;
         }
